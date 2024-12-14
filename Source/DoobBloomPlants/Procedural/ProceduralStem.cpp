f36b483436aeddadfb3fad2487f17ac2f29cd134
Zubin Falberg <the.z.creations@gmail.com>
2024-12-13 20:04:40 -0700

    preping project for adding cpp classes and created the procedural stem class

---
+++ Source/DoobBloomPlants/Procedural/ProceduralStem.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProceduralStem.h"
+
+// Sets default values
+AProceduralStem::AProceduralStem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProceduralStem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProceduralStem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
